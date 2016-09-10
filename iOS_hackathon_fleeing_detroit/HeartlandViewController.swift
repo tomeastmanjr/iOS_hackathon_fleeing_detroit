09282aca65d154a3827fa1e208cf13b0a07437bf
TomE <tom.eastman.jr@gmail.com>
2016-09-09 17:01:19 -0700

    final, final commit

--- iOS_hackathon_fleeing_detroit/HeartlandViewController.swift
+++ iOS_hackathon_fleeing_detroit/HeartlandViewController.swift
@@ -12,29 +12,36 @@ import UIKit
 class HeartlandViewController: UIViewController{
     
     @IBOutlet weak var healthLabel: UILabel!
+    var dead = false
     
-    
+    @IBOutlet weak var consoleLabel: UILabel!
     @IBAction func actionButtonPressed(sender: UIButton) {
-        if sender.tag == 1 {
-            bait -= 1
-            time -= 1
-        } else if sender.tag == 2 {
-            health += 10		
-            time -= 5
-        } else if sender.tag == 3 {
-            health -= 20
-            time -= 10
-            performSegueWithIdentifier("toMountains", sender: UIButton.self)
-        } else if sender.tag == 4 {
-            health -= 15
-            time -= 15
-            performSegueWithIdentifier("toMountains", sender: UIButton.self)
+        if dead == false{
+            if sender.tag == 1 {
+                bait -= 1
+                time -= 1
+            } else if sender.tag == 2 {
+                health += 10
+                time -= 5
+            } else if sender.tag == 3 {
+                health -= 20
+                time -= 10
+                performSegueWithIdentifier("toMountains", sender: UIButton.self)
+            } else if sender.tag == 4 {
+                health -= 15
+                time -= 15
+                performSegueWithIdentifier("toMountains", sender: UIButton.self)
+            }
+            updateUI()
         }
-        updateUI()
     }
     
     func updateUI(){
         healthLabel.text = "Health: \(health)"
+        if health <= 0 {
+            dead = true
+            consoleLabel.text = "You are dead!! Restart the app!"
+        }
     }
     
     override func viewDidLoad() {
@@ -42,5 +49,6 @@ class HeartlandViewController: UIViewController{
         // Do any additional setup after loading the view, typically from a nib.
         print("HEALTH \(health)")
         healthLabel.text = "Health:\(health)"
+        dead = false
     }
 }
--- iOS_hackathon_fleeing_detroit/MountainsViewController.swift
+++ iOS_hackathon_fleeing_detroit/MountainsViewController.swift
@@ -14,41 +14,48 @@ class MountainsViewController: UIViewController{
     @IBOutlet weak var healthLabel: UILabel!
     @IBOutlet weak var consoleLabel: UILabel!
 
+    var dead = false
+    
     @IBAction func actionButtonPressed(sender: UIButton) {
-        if sender.tag == 1 {
-            ammo -= 2
-            time -= 5
-            let hunted = Int(arc4random_uniform(UInt32(101))+1)
-            if hunted < 50{
-                food += hunted
-                consoleLabel.text = "Nice Shot! You got \(hunted) food"
-                health -= 10
-            }else if hunted % 3 == 0{
-                consoleLabel.text = "You have been eaten by a bear. Oops!"
-                    health = 0
-            }else{
-                consoleLabel.text = "You Missed!"
-                health -= 10
+        if dead == false{
+            if sender.tag == 1 {
+                ammo -= 2
+                time -= 5
+                let hunted = Int(arc4random_uniform(UInt32(101))+1)
+                if hunted < 50{
+                    food += hunted
+                    consoleLabel.text = "Nice Shot! You got \(hunted) food"
+                    health -= 10
+                }else if hunted % 3 == 0{
+                    consoleLabel.text = "You have been eaten by a bear. Oops!"
+                        health = 0
+                }else{
+                    consoleLabel.text = "You Missed!"
+                    health -= 10
+                }
+                
+            } else if sender.tag == 2 {
+                health += 10
+                time -= 5
+            } else if sender.tag == 3 {
+                health -= 20
+                time -= 10
+                performSegueWithIdentifier("toTreasureCove", sender: UIButton.self)
+            } else if sender.tag == 4 {
+                health -= 15
+                time -= 15
+                performSegueWithIdentifier("toTreasureCove", sender: UIButton.self)
             }
-            
-        } else if sender.tag == 2 {
-            health += 10
-            time -= 5
-        } else if sender.tag == 3 {
-            health -= 20
-            time -= 10
-            performSegueWithIdentifier("toTreasureCove", sender: UIButton.self)
-        } else if sender.tag == 4 {
-            health -= 15
-            time -= 15
-            performSegueWithIdentifier("toTreasureCove", sender: UIButton.self)
+            updateUI()
         }
-        updateUI()
     }
     
     func updateUI(){
         healthLabel.text = "Health: \(health)"
-        
+        if health <= 0 {
+            dead = true
+            consoleLabel.text = "You are dead!! Restart the app!"
+        }
     }
     
     override func viewDidLoad() {
